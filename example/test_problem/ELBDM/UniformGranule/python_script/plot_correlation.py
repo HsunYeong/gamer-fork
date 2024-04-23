d0fa0c6fe2130dbacc00a3251f1418da8cde7812
HsunYeong <jason88697@gmail.com>
2024-04-23 11:36:37 +0800

    add analytical form

--- example/test_problem/ELBDM/UniformGranule/python_script/plot_correlation.py
+++ example/test_problem/ELBDM/UniformGranule/python_script/plot_correlation.py
@@ -31,16 +31,24 @@ for f in filename:
       sys.exit(1)
 
 
-t = np.array(t)
+t = np.array(t)*1000
 C = np.array(C)
+
+d = 0.00234224
+sigma = 5.10172
+
+# C(t) decay time scale, C(t_corr)=(1+2*0.35**2)**(-1.5)*C(t=0) = 0.72*C(t=0)
+t_corr = d/(2**0.5*np.pi*sigma)*1000
 
 color = plt.cm.turbo(np.linspace(0.1, 0.9, len(r[0])))
 
 plt.figure()
 for i in range(len(r[0])):
-   plt.plot(t, C[:,i], c=color[i], label = 'r = %1.3e'%(r[0][i]))
+   plt.plot(t, C[:,i], c=color[i], label = r'$r$ = %1.3e kpc'%(r[0][i]))
+plt.axvline(x=t_corr, ls='--', lw = '0.9', color = '0.7', label = 'expected correlation time scale')
 plt.xlabel('$t$ (Myr)')
 plt.ylabel(r'$C(t)$')
+plt.xlim(0, t[-1])
 plt.legend(bbox_to_anchor=(1.03,0.03), loc='lower left')
 plt.savefig('fig_correlation.png', dpi = 150, bbox_inches="tight")
 plt.close()
